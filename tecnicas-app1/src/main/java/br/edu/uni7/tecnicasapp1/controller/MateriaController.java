15c2a4a2b8978cff51a70aa8dc93f71457d2ba3f
Alex Ferreira <aferreira91@gmail.com>
2021-10-13 09:04:14 -0300

    spa

--- tecnicas-app1/src/main/java/br/edu/uni7/tecnicasapp1/controller/MateriaController.java
+++ tecnicas-app1/src/main/java/br/edu/uni7/tecnicasapp1/controller/MateriaController.java
@@ -4,9 +4,7 @@ import br.edu.uni7.tecnicasapp1.model.Materia;
 import br.edu.uni7.tecnicasapp1.repository.MateriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Date;
@@ -31,15 +29,13 @@ public class MateriaController {
         return modelAndView;
     }
 
+    @ResponseBody
     @RequestMapping(value = "criarMateria", method = RequestMethod.POST)
-    public ModelAndView criarNovaMateria(@RequestParam String titulo, @RequestParam String autor, @RequestParam String conteudo) {
-        Materia materia = new Materia(titulo, autor, conteudo, new Date());
+    public Materia criarNovaMateria(@RequestBody Materia materia) {
+        materia.setData(new Date());
         materiaRepository.create(materia);
 
-        ModelAndView modelAndView = new ModelAndView("materia");
-        modelAndView.addObject("materias", materiaRepository.read());
-
-        return modelAndView;
+        return materia;
     }
 
 }
--- tecnicas-app1/src/main/resources/static/js/script.js
+++ tecnicas-app1/src/main/resources/static/js/script.js
@@ -1,1 +1,26 @@
-alert("olá");
+
+function submeterFormulario() {
+    var titulo = document.getElementById("titulo").value;
+    var autor = document.getElementById("autor").value;
+    var conteudo = document.getElementById("conteudo").value;
+
+    var materia = {
+        'titulo' : titulo,
+        'autor' : autor,
+        'conteudo' : conteudo,
+    };
+
+    function reqListener () {
+        var materia = JSON.parse(this.responseText);
+        console.log(materia);
+
+        //construir blocos de materia...
+        //fazer append nos blocos do HTML
+    };
+
+    var oReq = new XMLHttpRequest();
+    oReq.onload = reqListener;
+    oReq.open("POST", "criarMateria", true);
+    oReq.setRequestHeader("Content-Type", "application/json");
+    oReq.send(JSON.stringify(materia));
+}
